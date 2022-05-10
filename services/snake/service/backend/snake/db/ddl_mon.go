c91ce32458025e6c8e3063908e17cb607978fbc7
e-postovalov <e-postovalov@yandex-team.ru>
2022-05-10 14:43:27 +0500

    mongo_start: add migration mechanism

--- services/snake/service/backend/snake/db/ddl_mon.go
+++ services/snake/service/backend/snake/db/ddl_mon.go
@@ -2,7 +2,7 @@ package db
 
 import "go.mongodb.org/mongo-driver/mongo/options"
 
-func CreateCollection(dbName string, name string, opts ...*options.CreateCollectionOptions) error {
+func createCollection(dbName string, name string, opts ...*options.CreateCollectionOptions) error {
 	client, disconnect, err := createClient()
 	ctx, cancel, err := connect(client, err)
 	if err != nil {
