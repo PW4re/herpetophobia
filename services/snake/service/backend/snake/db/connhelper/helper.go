53c1744c63239fd894254dd27e8cb7d2582c3ba1
Popostatopulos <egorpostovalov@gmail.com>
2022-05-15 19:01:19 +0500

    server: допушиваю

--- services/snake/service/backend/snake/db/connhelper/helper.go
+++ services/snake/service/backend/snake/db/connhelper/helper.go
@@ -15,7 +15,7 @@ var once sync.Once
 func GetClient() *mongo.Client {
 	var clientError error
 	once.Do(func() {
-		client, clientError = mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@127.0.0.1:27017/")) // stubbed
+		client, clientError = mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@mongo:27017/")) // stubbed
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		err := client.Connect(ctx) // significant panic reason
 		if err != nil {
